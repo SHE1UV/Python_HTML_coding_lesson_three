bbb31d08a2687bc7e518c3568aac0144b56a24a2
SHE1UV <126342687+SHE1UV@users.noreply.github.com>
2024-01-15 18:51:36 +0300

    Исправил ошибки из прошлого ревью

--- tululu.py
+++ tululu.py
@@ -1,92 +1,123 @@
 import os
 import sys
 import logging
-import argparse
 from time import sleep
-from urllib.parse import urljoin, urlsplit, unquote
+
+import argparse
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin, urlsplit, unquote  
 from pathvalidate import sanitize_filename
 
-logger = logging.getLogger(__name__)
+
+logger = logging.getLogger()
 
 
 def fetch_book_ids():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', default=1, type=int)
-    parser.add_argument('-e', default=10, type=int)
-    return parser.parse_args()
+    parser.add_argument('-s', '--start_id', default=1, type=int)
+    parser.add_argument('-e', '--end_id', default=10, type=int)
+    args = parser.parse_args()
+    return args.start_id, args.end_id
 
 
-def parse_book_page(html, url):
-    soup = BeautifulSoup(html, 'lxml')
-
-    return {
-        "title": title,
-        "author": author,
-        "image_url": image_url,
-        "comments": comments,
-        "genres": genres
-    }
+def check_for_redirect(response):
+    if response.history:
+        raise requests.exceptions.HTTPError
 
 
-def download_txt(url, payload, filename, folder):
+def download_txt(url, payload, filename, folder='books/'):
+    os.makedirs(folder, exist_ok=True)
     filepath = os.path.join(folder, sanitize_filename(f"{filename}.txt"))
-    with open(filepath, 'wb') as f:
-        f.write(requests.get(url, params=payload).content)
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    check_for_redirect(response)
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
+    return filepath
 
 
-def download_image(url, filename, folder):
-    filepath = os.path.join(folder, sanitize_filename(f"{filename}"))
-    with open(filepath, 'wb') as f:
-        f.write(requests.get(url).content)
+def download_image(url, filename, folder='images/'):
+    os.makedirs(folder, exist_ok=True)
+    filepath = os.path.join(folder, sanitize_filename(filename))
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response)
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
+    return filepath
+
+
+def parse_book_page(html):
+    soup = BeautifulSoup(html, 'lxml')
+    h1 = soup.find('h1')
+    title, author = h1.text.split('::')
+    img_tag = soup.find('div', class_='bookimage')
+    img_url = urljoin(url, img_tag.find('img')['src'])
+    comments = [span.text for span in soup.find_all('span', 'black')] 
+    genres = [a.text for a in soup.find('span', 'd_book').find_all('a')]
+    return {
+        'title': title,
+        'author': author,
+        'img_url': img_url,
+        'comments': comments,
+        'genres': genres
+        }
 
 
 def print_book_info(book):
-    print(book)
+    print(f"Title: {book['title']}")
+    print(f"Comments: {book['comments']}")
+
+
+def download_book(book_id):
+    url = f'https://tululu.org/b{book_id}/'
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response)
+    book = parse_book_page(response.text)
+    filename = f"{book_id}. {book['title']}"
+    txt_url = 'https://tululu.org/txt.php'
+    payload = {'id': book_id}
+    txt_path = download_txt(txt_url, payload, filename)
+    img_name = unquote(urlsplit(book['img_url']).path).split("/")[-1]
+    img_path = download_image(book['img_url'], img_name)
+    print_book_info(book)
 
 
 def main():
-    logging.basicConfig(filename='app.log', level=logging.INFO)
+    logging.basicConfig(level=logging.INFO, filename='errors.log', filemode='w')
 
     try:
-        start_id, end_id = fetch_book_ids()
+        start, end = fetch_book_ids()
+    except ValueError:
+        logger.error("Invalid book IDs")
+        sys.exit(1)
 
-        for book_id in range(start_id, end_id+1):
+    try:
+        for book_id in range(start, end + 1):
+            download_book(book_id)
             
-            url = f"https://tululu.org/b{book_id}/"
-            response = requests.get(url)
-
-            if response.history:
-                logger.warning(f"Книга {book_id} не найдена - редирект")
-                continue
-
-            book = parse_book_page(response.text, url)
-
-            download_txt(
-                "https://tululu.org/txt.php",
-                {"id": book_id},
-                f"{book_id}. {book['title']}",
-                "books"
-            )
-
-            download_image(
-                book['image_url'], 
-                unquote(urlsplit(book['image_url']).path).split("/")[-1],
-                "images"
-            )
-
-            print_book_info(book)
-
-    except requests.RequestException as e:
-        logger.error(f"Ошибка запроса: {e}")
+    except requests.exceptions.ConnectionError:
+        logger.error("Connection error")
+        sleep(5)
         
-    except FileNotFoundError as e:
-        logger.error(f"Не найден файл: {e}")
+        for book_id in range(start, end + 1):
+            try:
+                download_book(book_id)
+            except requests.exceptions.ConnectionError:
+                logger.error("Connection still error")
+                sleep(15)
+                
+                for book_id in range(start, end + 1):
+                    download_book(book_id)
+                    
+    except OSError as err:
+        logger.error(f"IO error: {err}")
+            
+    except requests.exceptions.HTTPError as err:
+        logger.error(f"HTTP error: {err}")
 
-    except OSError as e:
-        logger.error(f"Ошибка файловой системы: {e}")
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
